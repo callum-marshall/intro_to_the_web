734f8d213d406c8a289215ac57e5343e94e03b11
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-16 12:51:33 +0100

    separated routing and presentation concerns

--- app.rb
+++ app.rb
@@ -1,20 +1,8 @@
  require 'sinatra'
  set :session_secret, 'super secret'
 
- get '/' do
-   'Callum rules, ok!'
- end
-
-get '/secret' do
-   'Calum also rules, ok!'
-end
-
 get '/cat' do
-  "<div>
-    <img src='http://bit.ly/1eze8aE'
-    style='border: 3px dashed red'>
-  </div>"
-  #
+  erb(:index)
 end
 
 # get '/' do
