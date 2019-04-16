3ca5eaae28b563bab779f11f2491e8eafa5e21d3
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-04-16 14:44:29 +0100

    added params

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  @type = [" -a nice cat", " -a bad cat"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   @type = [" -a nice cat", " -a bad cat"].sample
   erb :index
 end
